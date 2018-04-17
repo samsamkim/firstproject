c217d6227794c4a5180638c3e39b27da42ad94ef
samsamkim <hoonshik10@gmail.com>
2018-04-17 00:27:50 +0000

    updating header

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,28 @@
 var request = require('request');
 
+// "Authorization", + 9cf67734e36c67dc16cc202e93555254b906b7fe
+
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token 9cf67734e36c67dc16cc202e93555254b906b7fe'
+    }
+  };
+
+  request(options, function(err, res, body) {
+
+    cb(err, body);
+  });
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
