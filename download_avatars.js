e5ee70897edf1116cc1b37ea6193a41b49867970
samsamkim <hoonshik10@gmail.com>
2018-04-17 02:37:51 +0000

    made the arguments required

--- download_avatars.js
+++ download_avatars.js
@@ -2,17 +2,23 @@ var request = require('request');
 var GITHUB_TOKEN = require('./secrets.js');
 var fs = require('fs');
 
+if(process.argv.length !== 4){
+  return console.log("Please input a repo owner and a repo name!");
+}
+
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-
 function downloadImageByURL(url, filePath) {
   request.get(url)
+         // .on('error', function (err) {
+         //   throw err;})
          .pipe(fs.createWriteStream(filePath));
 }
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
+
   repoOwner = process.argv[2];
   repoName = process.argv[3];
   var options = {
@@ -24,14 +30,15 @@ function getRepoContributors(repoOwner, repoName, cb) {
   };
 
   request(options, function(err, res, body) {
-    var parsed = JSON.parse(body);
 
+    var parsed = JSON.parse(body);
     var url = parsed.forEach(function(profile) {
       downloadImageByURL(profile.avatar_url, ("avatars/" + profile.login + ".jpg"));
     });
     cb(err, url);
 
   });
+
 }
 
 
