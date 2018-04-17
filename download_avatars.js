704e9c9e4fd9da01654153e3c1522ac13508d208
samsamkim <hoonshik10@gmail.com>
2018-04-17 01:40:33 +0000

    added downloadimagebyurl and implemented it in getrepocontributors

--- download_avatars.js
+++ download_avatars.js
@@ -1,8 +1,15 @@
 var request = require('request');
 var GITHUB_TOKEN = require('./secrets.js');
+var fs = require('fs');
+
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+         .pipe(fs.createWriteStream(filePath));
+}
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
@@ -15,10 +22,21 @@ function getRepoContributors(repoOwner, repoName, cb) {
   };
 
   request(options, function(err, res, body) {
+    var parsed = JSON.parse(body);
 
-    cb(err, body);
+    var url = parsed.forEach(function(profile) {
+      downloadImageByURL(profile.avatar_url, ("avatars/" + profile.login + ".jpg"));
+    });
+
+    cb(err, url);
+
   });
 }
+
+
+
+
+
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
